085b1afaa2bf551798712b8e02794d2ffffa35b3
redy <redy.ru@gmail.com>
2019-05-10 12:51:06 +0800

    xxx

--- server/deploy/prd_totoro_admin.py
+++ server/deploy/prd_totoro_admin.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import multiprocessing
 
 bind = '127.0.0.1:9009'
-workers = min(multiprocessing.cpu_count(), 3)
+workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = '/data/deployment_data/totoro/log/totoro_admin.access.log'
 errorlog = '/data/deployment_data/totoro/log/totoro_admin.error.log'
 pidfile = '/data/deployment_data/totoro/totoro_admin.pid'
